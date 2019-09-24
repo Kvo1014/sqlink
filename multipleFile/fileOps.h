cf44b653e976002295ea9845fd14a08c79a7ae33
stasvasilyev <36970796+stasvasilyev@users.noreply.github.com>
2019-09-24 15:22:29 +0300

    Delete fileOps.h

--- multipleFile/fileOps.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILESOPS_H
-#define FILESOPS_H
-
-int isFile (const char* path);
-int isDir (const char* path);
-char** getContent (char* path);
-void freeContent (char** content);
-
-#endif
