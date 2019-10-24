7684499a97d24724981e4636dda42b159c279c5f
stasvasilyev <36970796+stasvasilyev@users.noreply.github.com>
2019-10-24 12:53:41 +0300

    Add files via upload

---
+++ Cpp/string2/inputOutput/virtio.cpp
@@ -0,0 +1,87 @@
+#include "virtIO_t.h"
+
+
+void VirtIO_t::open()
+{
+    if(m_access == "r" || m_access == "r+" || m_access == "w" || m_access == "w+")
+    {
+        m_fp = fopen(m_name.c_str(), m_access.c_str());
+        if(!m_fp)
+        {
+            m_status = cant_open_file_e;
+            throw string("error open file");
+        }
+    }
+    else
+    {
+        m_status = bad_access_e;
+        throw string("bad access");
+    }    
+}
+
+VirtIO_t::VirtIO_t()
+{
+    m_status = ok_e;
+    m_fp = 0;
+}
+
+VirtIO_t::VirtIO_t(const string& name, const string& access)
+{
+    m_name = name;
+    m_access = access;
+    m_status = ok_e;
+    m_fp = 0;
+    open();
+}
+
+
+
+void VirtIO_t::close()
+{
+    if(m_fp)
+    {
+        fclose(m_fp);
+        m_fp = 0;
+    }
+}
+
+ size_t VirtIO_t::getPosition() const
+ {
+    size_t position = 0;
+    if(m_fp)
+    {        
+        position = ftell(m_fp);
+    }
+ }
+
+
+size_t VirtIO_t::getFileLenght() const
+{
+    size_t lenght = 0, currPosition = ftell(m_fp);
+
+    if(m_fp)
+    {
+        fseek(m_fp, 0, SEEK_END);
+        lenght = ftell(m_fp);
+        fseek(m_fp, currPosition, SEEK_SET);
+    }
+    return lenght;
+}
+
+void VirtIO_t::setPosition(size_t position)
+{
+    if(m_fp)
+    {
+        fseek(m_fp, position, SEEK_SET);
+    }
+}
+
+bool VirtIO_t::validRead()
+{
+    return (!m_fp || (m_access != "r" && m_access != "r+" && m_access != "w+")) ? false : true;
+}
+
+bool VirtIO_t::validWrite()
+{
+    return (!m_fp || (m_access != "w" && m_access != "w+" && m_access != "r+")) ? false : true;
+}
