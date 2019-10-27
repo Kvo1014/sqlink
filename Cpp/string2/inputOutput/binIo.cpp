34d3ab20e960a1ef878b62005a947f1774ed5db4
stasvasilyev <36970796+stasvasilyev@users.noreply.github.com>
2019-10-27 10:57:15 +0200

    Add files via upload

---
+++ Cpp/string2/inputOutput/binIo.cpp
@@ -0,0 +1,65 @@
+#include "binIO_t.h"
+
+BinIO_t& BinIO_t::operator>>(int& val)
+{
+    return fileToVal<int>(val);
+}
+
+BinIO_t& BinIO_t::operator<<(int val)
+{
+    return valToFile<int>(val);
+}
+
+BinIO_t& BinIO_t::operator>>(float& val)
+{
+    return fileToVal<float>(val);
+}
+
+BinIO_t& BinIO_t::operator<<(float val)
+{
+    return valToFile<float>(val);
+}
+
+
+BinIO_t& BinIO_t::operator>>(void* buf)
+{
+    m_rightShift = buf;
+    return *this;
+}
+
+BinIO_t& BinIO_t::operator<<(const void* buf)
+{
+    m_leftShift = (void*)buf;
+    return *this;
+}
+
+void BinIO_t::operator,(int len)
+{
+    if(m_rightShift)
+    {
+        if(validRead())
+        {
+            fread(m_rightShift, len, 1, m_fp);
+            m_rightShift = 0;
+        }
+        
+    else
+        {
+            m_status = readErr_e;
+            throw string("Read error");
+        }
+    }
+    else if(m_leftShift)
+    {
+        if(validWrite())
+        {
+            fwrite(m_leftShift, len, 1, m_fp);
+            m_leftShift = 0;
+        }
+        else
+        {
+            m_status = writeErr_e;
+            throw string("Write error");
+        }
+    }
+}
